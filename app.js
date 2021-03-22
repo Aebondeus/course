5859a000b2fd4e5d42750b2c5c17b8ed2b0787f9
Aebondeus <Aebondeus@yandex.ru>
2021-03-22 12:05:57 +0300

    for deploy v1

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(passport.initialize());//
 app.use(passport.session());//
 
 app.use(cors({//
-  origin:"http://localhost:3000/",
+  origin:"mordorcourse.herokuapp.com",
   methods:"GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials:true
 }))
@@ -63,8 +63,8 @@ const start = async () => {
 start();
 
 if (process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "frontend", "build")));
+  app.use(express.static(path.join(__dirname, "client", "build")));
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   })
 }
