944baaa721343fc4605ba3cbe4f07067d236c277
Aebondeus <Aebondeus@yandex.ru>
2021-03-23 20:25:04 +0300

    for deploy v1

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,10 +1,9 @@
 import cloudinary from "cloudinary";
-import config from "../config";
 const cloud = cloudinary.v2;
 cloud.config({ // set it all as process.env
-    cloud_name: process.env.CLOUD_NAME || config.CLOUD_NAME, 
-    api_key: process.env.CLOUD_KEY || config.CLOUD_KEY, 
-    api_secret: process.env.CLOUD_SECRET || config.CLOUD_SECRET
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_KEY,
+    api_secret: process.env.CLOUD_SECRET
 })
 
 export default cloud;
